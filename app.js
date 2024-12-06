1b86d67efd91b89ef945039075ac138e53627090
c9czamat <c9czamat@vasvari.org>
2024-12-06 10:15:33 +0100

    swagger működik

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ import express from "express";
 import cors from "cors";
 import { initializeDB } from "./database.js";
 import usersRouter from "./routes/users.js";
+import swaggerUi from 'swagger-ui-express';
+import { readFile } from "fs/promises";
 
 const app = express();
 
@@ -19,5 +21,6 @@ const startServer = async () => {
     await initializeDB();
     app.listen(3000, () => console.log("Server is running on port 3000"));
 };
-
+const swaggerDocument = JSON.parse(await readFile(new URL("./swagger-output.json", import.meta.url)));
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 startServer();
